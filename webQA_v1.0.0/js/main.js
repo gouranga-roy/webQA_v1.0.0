a4dd41e30af4e5396db76965ea7ec0df2a9d91c2
Gouranga Roy <gourangaroy831@gmail.com>
2022-12-18 09:50:16 -0800

    change

--- webQA_v1.0.0/js/main.js
+++ webQA_v1.0.0/js/main.js
@@ -272,4 +272,4 @@ $(document).ready(function () {
 
 // Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
