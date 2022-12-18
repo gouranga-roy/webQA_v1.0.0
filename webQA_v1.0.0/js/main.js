35c92e0ee72c0550e6097b77432267cf62bb9a44
Gouranga Roy <gourangaroy831@gmail.com>
2022-12-17 19:48:39 -0800

    modyfi

--- webQA_v1.0.0/js/main.js
+++ webQA_v1.0.0/js/main.js
@@ -269,3 +269,7 @@ $(document).ready(function () {
     }
   });
 });
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
